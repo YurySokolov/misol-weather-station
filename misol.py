9af0c11a0100ef2d8d3ee3d118aff914ae9aa865
YurySokolov <igrolyb@gmail.com>
2020-03-27 18:17:08 +0700

    now it works

--- misol.py
+++ misol.py
@@ -23,7 +23,7 @@ def publish(client: Client, topic: str, order: int):
 
 
 logging.basicConfig(
-    level=logging.DEBUG, filename='/tmp/misol.log', filemode='w',
+    level=logging.DEBUG, filename='/misol/log.log', filemode='w',
     format="%(asctime)s %(levelname)s [%(name)s] %(message)s")
 log = logging.getLogger(__name__)
 
@@ -36,18 +36,7 @@ UVI = [432, 851, 1210, 1570, 2017, 2761, 3100, 3512, 3918, 4277, 4650, 5029,
 
 def main():
     try:
-        client = Client("Misol")  # create new instance
-        client.connect("192.168.1.3")  # connect to broker
-
-        client.publish("/devices/misol/meta/name", "Misol Meteostation", qos=0,
-                       retain=True)
-
-        for order, topic in enumerate(TOPICS, 1):
-            publish(client, topic, order)
-
-        log.debug("Connected to MQTT")
-
-        s = Serial('/dev/ttyUSB0', 9600, timeout=60)
+        s = Serial('COM3', 9600, timeout=60)
 
         log.debug("Connected to serial")
 
@@ -59,31 +48,38 @@ def main():
             checksum = sum(i for i in raw[:16]) & 0xFF
             assert checksum == raw[16], "Wrong checksum"
 
-            wd = wdata.from_buffer_copy(raw)
-            rwd: RawWeatherData = wd.rawdata
-
-            wind = ((wd.rawdata.WSP8 << 8) + wd.rawdata.WIND) / 8 * 1.12
-            uvi = next((i for i, v in enumerate(UVI) if rwd.UVI <= v), 13)
+            TX = raw[0]
+            SC = raw[1]
+            WD = raw[2]
+            TP8 = raw[3] & 0b00001111
+            TP = (((TP8 << 8) + raw[4]) - 400) / 10
+            HM = raw[5]
+            WS = (raw[6] / 8 ) * 1.12
+            WG = raw[7] * 1.12
+            RN = (raw[8] << 8) + raw[9]
+            UV = (raw[10] << 8) + raw[11]
+            LT = ((raw[12] << 16) + (raw[13] << 8) + (raw[14]))/10
+            BT = raw[15]
+            PS = ((raw[17] << 16) + (raw[18] << 8) + (raw[19]))/100
 
             payload = {
-                'wind_direction': (wd.rawdata.DIR8 << 8) + wd.rawdata.DIR,
-                'battery_low': rwd.BAT,
-                'temperature': (rwd.TMP - 400) / 10.0,
-                'humidity': rwd.HM,
-                'wind_speed': round(wind),
-                'wind_gust': round(rwd.GUST * 1.12),
-                'rain': rwd.RAIN,
-                'uvi': uvi,
-                'light': round(rwd.LIGHT / 10.0),
-                'bar': round(rwd.BAR / 100.0, 2),
-                'last_update': int(time.time())
-            }
+                    'wind_direction': WD,
+                    'humidity': HM,
+                    'temperature' : TP,
+                    'wind_speed' : WS,
+                    'wind_gusts' : WG,
+                    'rain' : RN,
+                    'uvi' : UV,
+                    'light' : LT,
+                    'battery' : BT,
+                    'pressure' : PS,
+                    'last_update': int(time.time())
+                }
 
             for k, v in payload.items():
-                info = client.publish(f"{BASE}/{k}", v, retain=True)
-                assert info.rc == MQTT_ERR_SUCCESS, f"MQTT Error: {info.rc}"
+                log.debug(k + ":" + str(v))
 
-            log.debug("Updated MQTT")
+            log.debug("Got info")
 
     except AssertionError as e:
         log.error(e)
--- wdata.py
+++ wdata.py
@@ -7,11 +7,11 @@ class RawWeatherData(BigEndianStructure):
         ("FC", c_uint8, 8),
         ("SC", c_uint8, 8),
         ("DIR", c_uint8, 8),
-        ("DIR8", c_uint8, 1),
-        ("FIX", c_uint8, 2),
-        ("WSP8", c_uint8, 1),
-        ("BAT", c_uint8, 1),
-        ("TMP", c_uint16, 11),
+        ("DIR8", c_uint8, 4),
+        # ("FIX", c_uint8, 2),
+        # ("WSP8", c_uint8, 1),
+        # ("BAT", c_uint8, 1),
+        ("TMP", c_uint16, 12),
         ("HM", c_uint8, 8),
         ("WIND", c_uint8, 8),
         ("GUST", c_uint8, 8),
@@ -19,14 +19,13 @@ class RawWeatherData(BigEndianStructure):
         ("UVI", c_uint16, 16),
         ("LIGHT", c_uint32, 24),
         ("CRC", c_uint8, 8),
-        ("CHECKSUM", c_uint8, 8),
-        ("BAR", c_uint32, 24),
-        ("CHECKSUM_BAR", c_uint8, 8)
+        ("CHECKSUM", c_uint8, 8)
+        # ("BAR", c_uint32, 24),
+        # ("CHECKSUM_BAR", c_uint8, 8)
     ]
 
 class wdata(Union):
     _pack_ = 1
     _fields_ = [
-        ("rawdata", RawWeatherData),
-        ("ulData", c_uint64)
+        ("rawdata", RawWeatherData)
     ]
